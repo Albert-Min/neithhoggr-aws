705136db0fa4499224c060f20a644c5dfbb7431b
Albert Min <sixiang.min@rea-group.com>
2023-06-06 17:18:18 +0800

    feat: add auto scaling group

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,3 @@ import { ERCRepo } from './type';
 export const DEFAULT_MAX_ECR_IMAGE_COUNT = 100;
 
 export const ECR_REPOS: ERCRepo[] = [{ name: 'neithhoggr-openai-api' }];
-
-export const ECS_CLUSTER_NAME = 'ecs-neithhoggr';
-
-export const VPC_NAME = 'vpc-neithhoggr';
--- lib/ecs.ts
+++ lib/ecs.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import { Construct } from 'constructs';
 import { ECS_CLUSTER_NAME, VPC_NAME } from './constants';
 
@@ -9,13 +10,25 @@ export class EcsClusterStack extends cdk.Stack {
     super(scope, id, props);
 
     // Create a VPC
-    const vpc = new ec2.Vpc(this, VPC_NAME, {
+    const vpc = new ec2.Vpc(this, "VPC", {
       maxAzs: 3, // Default is all AZs in the region
     });
 
     // Create an ECS cluster
-    new ecs.Cluster(this, ECS_CLUSTER_NAME, {
+    const cluster = new ecs.Cluster(this, "Cluster", {
       vpc: vpc,
     });
+
+    // Create an Auto Scaling group
+    const autoScalingGroup = new autoscaling.AutoScalingGroup(this, 'AutoScalingGroup', {
+      vpc: vpc,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ecs.EcsOptimizedAmi(),
+      desiredCapacity: 3,
+      // other Auto Scaling group properties...
+    });
+
+    // Add the Auto Scaling group to the ECS cluster
+    cluster.addAutoScalingGroupCapacity(autoScalingGroup);
   }
 }
