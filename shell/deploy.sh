7770316aaf60d643b1eb7ed0e42bdd2148dd1383
Albert Min <sixiang.min@rea-group.com>
2023-06-06 16:24:02 +0800

    fix: delete diff script

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -3,16 +3,10 @@
 # Exit if any command fails
 set -e
 
-# Run cdk diff and capture the output
-output=$(npx cdk diff)
+npx cdk diff
 
-# Check if the output contains the string "There were no differences"
-if [[ $output == *"There were no differences"* ]]; then
-  echo "No changes to deploy."
-else
-  echo "Changes detected. Running cdk bootstrap and cdk deploy."
-  npx cdk bootstrap
-  npx cdk deploy --all --require-approval never
-fi
+npx cdk bootstrap
+
+npx cdk deploy --all --require-approval never
 
 echo "Deployment complete"
