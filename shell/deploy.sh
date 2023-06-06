fb51e6466d0a1e9789e21bbc87ce7de2506bb95b
Albert Min <sixiang.min@rea-group.com>
2023-06-06 14:40:57 +0800

    chore: update deploy.sh & add cache

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -3,11 +3,16 @@
 # Exit if any command fails
 set -e
 
-npx cdk diff
+# Run cdk diff and capture the output
+output=$(npx cdk diff)
 
-npx cdk bootstrap
-
-npx cdk deploy
-
+# Check if the output contains the string "There were no differences"
+if [[ $output == *"There were no differences"* ]]; then
+  echo "No changes to deploy."
+else
+  echo "Changes detected. Running cdk bootstrap and cdk deploy."
+  npx cdk bootstrap
+  npx cdk deploy
+fi
 
 echo "Deployment complete"
