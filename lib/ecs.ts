4dee5eef7914a2867c8e6d3cd0cb5135e6561232
Albert Min <sixiang.min@rea-group.com>
2023-06-06 17:20:18 +0800

    fix: delete useless imports

--- lib/ecs.ts
+++ lib/ecs.ts
@@ -3,7 +3,6 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import { Construct } from 'constructs';
-import { ECS_CLUSTER_NAME, VPC_NAME } from './constants';
 
 export class EcsClusterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
