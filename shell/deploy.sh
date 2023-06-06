cacdb570d5c6481d1e6ceceb602ac06ae56b6843
Albert Min <sixiang.min@rea-group.com>
2023-06-06 14:35:48 +0800

    feat: update deploy.sh

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -3,16 +3,11 @@
 # Exit if any command fails
 set -e
 
-# Run cdk diff and capture the output
-output=$(npx cdk diff)
+npx cdk diff
 
-# Check if the output contains the string "No changes"
-if [[ $output == *"No changes"* ]]; then
-  echo "No changes to deploy."
-else
-  echo "Changes detected. Running cdk bootstrap and cdk deploy."
-  npx cdk bootstrap
-  npx cdk deploy
-fi
+npx cdk bootstrap
+
+npx cdk deploy
+
 
 echo "Deployment complete"
