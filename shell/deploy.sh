2fa40ddc3bd837779487397c0585e4e61b532514
Albert Min <sixiang.min@rea-group.com>
2023-06-06 16:51:45 +0800

    fix: optmize deploy.sh

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# Exit if any command fails
-set -e
+# List of your stack names
+stacks=("NeithhoggrECRStack" "NeithhoggrECSClusterStack")
 
-# Run cdk diff and capture the output
-output=$(npx cdk diff)
-
-echo $output
-
+# Run cdk bootstrap
 npx cdk bootstrap
 
-npx cdk deploy --all --require-approval never
+# Loop over the stacks
+for stack in "${stacks[@]}"
+do
+  # Run cdk diff for the current stack and capture the output
+  output=$(npx cdk diff $stack)
 
-echo "Deployment complete"
+  # Check if the output contains the string "There were no differences"
+  if [[ $output == *"There were no differences"* ]]; then
+    echo "No changes to deploy for $stack."
+  else
+    echo "Changes detected in $stack, running npx cdk deploy."
+    npx cdk deploy $stack
+  fi
+done
