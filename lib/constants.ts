8dab8ba6984982a7afec36b9b2e9f554234e0113
Albert Min <sixiang.min@rea-group.com>
2023-06-07 13:06:15 +0800

    chore: give ecs task execution role a name

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,3 +5,5 @@ export const DEFAULT_MAX_ECR_IMAGE_COUNT = 100;
 export const ECR_REPOS: ERCRepo[] = [{ name: 'neithhoggr-openai-api' }];
 
 export const ECS_CLUSTER_NAME = 'ecs-cluster-neithhoggr';
+
+export const ECS_TASK_EXECUTION_ROLE = 'role-ecs-task-neithhoggr';
--- lib/iam.ts
+++ lib/iam.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
+import { ECS_TASK_EXECUTION_ROLE } from './constants';
 
 export class IAMStack extends cdk.Stack {
   constructor(scope: Construct, id: string) {
@@ -8,6 +9,7 @@ export class IAMStack extends cdk.Stack {
 
     // Create a new IAM role for ECS tasks
     const ecsTaskExecutionRole = new iam.Role(this, 'ECSTaskExecutionRole', {
+      roleName: ECS_TASK_EXECUTION_ROLE, // specify the role name here
       assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
     });
 
