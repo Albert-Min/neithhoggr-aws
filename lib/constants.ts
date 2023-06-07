39132963c625fe3812f426d4a432aa94625ede94
Albert Min <sixiang.min@rea-group.com>
2023-06-07 14:46:31 +0800

    feat: add ecs services

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,6 +6,6 @@ export const ECR_REPOS: ERCRepo[] = [{ name: 'neithhoggr-openai-api' }];
 
 export const ECS_CLUSTER_NAME = 'ecs-cluster-neithhoggr';
 
-export const ECS_TASK_EXECUTION_ROLE_NAME =
-  'role-ecs-task-execution-neithhoggr';
-export const ECS_TASK_ROLE_NAME = 'role-ecs-task-neithhoggr';
+export const ECS_SERVICE_OPENAI_API_NAME = 'neithhoggr-openai-api';
+
+export const ECS_TASK_NODE_FAMILY_NAME = 'neithhoggr-node-task-defn';
--- lib/ecs.ts
+++ lib/ecs.ts
@@ -1,8 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
-import { ECS_CLUSTER_NAME } from './constants';
+import { ApplicationLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import {
+  ECS_CLUSTER_NAME,
+  ECS_SERVICE_OPENAI_API_NAME,
+  ECS_TASK_NODE_FAMILY_NAME,
+} from './constants';
+import { getECSExecutionRole } from './iams/getECSRoles';
 
 export class EcsClusterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,11 +20,44 @@ export class EcsClusterStack extends cdk.Stack {
       maxAzs: 3, // Default is all AZs in the region
     });
 
+    const loadbalancer = new ApplicationLoadBalancer(this, 'lb', {
+      vpc,
+      internetFacing: true,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PUBLIC,
+      }),
+    });
+
     // Create an ECS cluster
     const cluster = new ecs.Cluster(this, 'Cluster', {
       vpc: vpc,
       clusterName: ECS_CLUSTER_NAME,
     });
+
+    const executionRole = getECSExecutionRole(this);
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      'FargateNodeService',
+      {
+        cluster,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+          containerName: 'app',
+          family: ECS_TASK_NODE_FAMILY_NAME,
+          containerPort: 3000,
+          executionRole,
+        },
+        cpu: 256,
+        memoryLimitMiB: 512,
+        desiredCount: 2,
+        serviceName: ECS_SERVICE_OPENAI_API_NAME,
+        taskSubnets: vpc.selectSubnets({
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        }),
+        loadBalancer: loadbalancer,
+      },
+    );
 
     // Enable Fargate capacity providers
     cluster.enableFargateCapacityProviders();
