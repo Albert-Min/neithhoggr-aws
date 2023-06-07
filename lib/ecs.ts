14a90ae3a2df7bab62ff410129f65ec500e7f634
Albert Min <sixiang.min@rea-group.com>
2023-06-07 12:38:15 +0800

    chore: enable Fargate capacity providers

--- lib/ecs.ts
+++ lib/ecs.ts
@@ -1,7 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
-import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import { Construct } from 'constructs';
 import { ECS_CLUSTER_NAME } from './constants';
 
@@ -14,28 +13,13 @@ export class EcsClusterStack extends cdk.Stack {
       maxAzs: 3, // Default is all AZs in the region
     });
 
-    const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T3,
-        ec2.InstanceSize.MICRO,
-      ),
-      machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
-      desiredCapacity: 3,
-      vpc,
-    });
-
     // Create an ECS cluster
     const cluster = new ecs.Cluster(this, 'Cluster', {
       vpc: vpc,
       clusterName: ECS_CLUSTER_NAME,
     });
 
-    // Add capacity provider to the ECS cluster
-    const capacityProvider = new ecs.AsgCapacityProvider(
-      this,
-      'AsgCapacityProvider',
-      { autoScalingGroup: asg },
-    );
-    cluster.addAsgCapacityProvider(capacityProvider);
+    // Enable Fargate capacity providers
+    cluster.enableFargateCapacityProviders();
   }
 }
